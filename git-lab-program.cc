53824dbcc68add77ae9bfb5184971bbb9055fe47
htetlak <ht607919@ohio.edu>
2020-08-27 13:49:25 -0400

    Finished

---
+++ git-lab-program.cc
@@ -0,0 +1,16 @@
+/*
+ * file: git-lab-program.cc
+ * Author: Henry Tetlak
+ * Date 8/27/2020
+ * Description: Simple program that prints a small message 
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const*argv[]) {
+	cout << "Hello Git" << endl;
+	return (0);
+}//main
